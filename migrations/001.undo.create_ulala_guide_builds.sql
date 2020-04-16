843bbbd684a3773fca9f37e3fba1df5d8fb912e3
Douglas Miner <curr3nas@gmail.com>
2020-04-16 12:53:44 -0500

    databases seeded and tables created

---
+++ migrations/001.undo.create_ulala_guide_builds.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS ulala_guide_builds;
+
+DROP TYPE IF EXISTS
+  class_skills,
+  class_clatters,
+  class_tempers,
+  class_whishes,
+  class_name,
+  class_attributes;
---
+++ migrations/002.undo.create_ulala_guide_users.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS ulala_guide_users;
